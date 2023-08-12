e34c61dc4beba3d039df8d55e86a95fd8da8ff94
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-08-13 00:15:30 +0300

    HW #36 next methods

--- src/telran/employees/service/CompanyImpl.java
+++ src/telran/employees/service/CompanyImpl.java
@@ -1,30 +1,52 @@
 package telran.employees.service;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 import telran.employees.dto.DepartmentSalary;
 import telran.employees.dto.Employee;
 import telran.employees.dto.SalaryDistribution;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.time.LocalDate;
 import java.util.*;
-
 public class CompanyImpl implements Company {
   LinkedHashMap<Long, Employee> employees = new LinkedHashMap<>();
+  TreeMap<Integer, Collection<Employee>> employeesSalary = new TreeMap<>();
 	@Override
 	public boolean addEmployee(Employee empl) {
+		boolean res = false;
+		Employee emplRes = employees.putIfAbsent(empl.id(), empl);
+		if(emplRes == null) {
+			res = true;
+			addEmployeeSalary(empl);
+		}
+		return  res;
+	}
+
+	private void addEmployeeSalary(Employee empl) {
+		int salary = empl.salary();
+		employeesSalary.computeIfAbsent(salary, k -> new HashSet<>()).
+		add(empl);
 		
-		return employees.putIfAbsent(empl.id(), empl) == null;
 	}
 
 	@Override
 	public Employee removeEmployee(long id) {
+		Employee res = employees.remove(id);
+		if(res != null) {
+			removeEmployeeSalary(res);
+		}
+		return res;
+	}
+
+	private void removeEmployeeSalary(Employee empl) {
+		int salary = empl.salary();
+		Collection<Employee> employeesCol = employeesSalary.get(salary);
+		employeesCol.remove(empl);
+		if(employeesCol.isEmpty()) {
+			employeesSalary.remove(salary);
+		}
 		
-		return employees.remove(id);
 	}
 
 	@Override
@@ -48,55 +70,74 @@ public class CompanyImpl implements Company {
 				.entrySet().stream().map(e -> new DepartmentSalary(e.getKey(),
 						e.getValue())).toList();
 	}
-	
+
 	@Override
 	public List<SalaryDistribution> getSalaryDistribution(int interval) {
-	    List<SalaryDistribution> result = new ArrayList<>();
-	    List<Employee> employeeList = new ArrayList<>(employees.values());
-
-	    int minSalary = employeeList.stream()
-	            .mapToInt(Employee::salary)
-	            .min()
-	            .orElse(0);
-	    int maxSalary = employeeList.stream()
-	            .mapToInt(Employee::salary)
-	            .max()
-	            .orElse(0);
-
-	    int range = maxSalary - minSalary;
-	    int intervals = range / interval + 1;
-
-	    for (int i = 0; i < intervals; i++) {
-	        int lowerBound = minSalary + i * interval;
-	        int upperBound = Math.min(minSalary + (i + 1) * interval - 1, maxSalary);
-
-	        long amountEmployeesInInterval = employeeList.stream()
-	                .filter(employee -> employee.salary() >= lowerBound && employee.salary() <= upperBound)
-	                .count();
-
-	        result.add(new SalaryDistribution(lowerBound, upperBound, (int) amountEmployeesInInterval));
-	    }
-
-	    return result;
+		
+		return employees.values().stream()
+				.collect(Collectors.groupingBy(e -> e.salary()/interval,
+						Collectors.counting()))
+				.entrySet().stream()
+				.map(e -> new SalaryDistribution(e.getKey() * interval,
+						e.getKey() * interval + interval - 1, e.getValue().intValue()))
+				.sorted((sd1, sd2) -> Integer.compare(sd1.minSalary(), sd2.minSalary()))
+						.toList();
 	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public void restore(String filePath) {
-	    try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filePath))) {
-	        employees = (LinkedHashMap<Long, Employee>) inputStream.readObject();
-	    } catch (IOException | ClassNotFoundException e) {
-	        e.printStackTrace();
-	    }
-	}
+////36
 
 	@Override
-	public void save(String filePath) {
-	    try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filePath))) {
-	        outputStream.writeObject(employees);
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
+	public List<Employee> getEmployeesByDepartment(String department) {
+	    return employees.values().stream()
+	            .filter(e -> e.department().equalsIgnoreCase(department))
+	            .toList();
 	}
+
+@Override
+public List<Employee> getEmployeesBySalary(int salaryFrom, int salaryTo) {
+    return employees.values().stream()
+            .filter(e -> e.salary() >= salaryFrom && e.salary() <= salaryTo)
+            .sorted(Comparator.comparingLong(Employee::id))
+            .toList();
+}
+	
+
+	@Override
+	public List<Employee> getEmployeesByAge(int ageFrom, int ageTo) {
+	    LocalDate currentDate = LocalDate.now();
+	    LocalDate birthDateFrom = currentDate.minusYears(ageTo);
+	    LocalDate birthDateTo = currentDate.minusYears(ageFrom).minusDays(1);
+
+	    return employees.values().stream()
+	            .filter(e -> e.birthDate().isAfter(birthDateFrom) && e.birthDate().isBefore(birthDateTo))
+	            .toList();
+	}
+
+
+	@Override
+	public Employee updateSalary(long id, int newSalary) {
+	    Employee employee = employees.get(id);
+	    if (employee != null) {
+	        removeEmployeeSalary(employee);
+	        employee = new Employee(employee.id(), employee.name(), employee.department(),
+	                newSalary, employee.birthDate());
+	        addEmployeeSalary(employee);
+	        employees.put(id, employee);
+	    }
+	    return employee;
+	}
+
+	@Override
+	public Employee updateDepartment(long id, String newDepartment) {
+	    Employee employee = employees.get(id);
+	    if (employee != null) {
+	        removeEmployeeSalary(employee);
+	        employee = new Employee(employee.id(), employee.name(), newDepartment,
+	                employee.salary(), employee.birthDate());
+	        addEmployeeSalary(employee);
+	        employees.put(id, employee);
+	    }
+	    return employee;
+	}
+	
 
 }
--- src/telran/employees/test/CompanyTest.java
+++ src/telran/employees/test/CompanyTest.java
@@ -3,7 +3,8 @@ package telran.employees.test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
@@ -18,6 +19,7 @@ import telran.employees.service.CompanyImpl;
 class CompanyTest {
 	private static final long ID1 = 123;
 	private static final String DEP1 = "dep1";
+	private static final int SALARY1 = 10000;
 	private static final int YEAR1 = 2000;
 	private static final LocalDate DATE1 = LocalDate.ofYearDay(YEAR1, 100);
 	private static final long ID2 = 124;
@@ -26,7 +28,6 @@ class CompanyTest {
 	private static final long ID5 = 127;
 	private static final String DEP2 = "dep2";
 	private static final String DEP3 = "dep3";
-	private static final int SALARY1 = 10000;
 	private static final int SALARY2 = 5000;
 	private static final int SALARY3 = 15000;
 	private static final int YEAR2 = 1990;
@@ -40,6 +41,7 @@ class CompanyTest {
 	Employee empl3 = new Employee(ID3, "name", DEP1, SALARY1, DATE1);
 	Employee empl4 = new Employee(ID4, "name", DEP2, SALARY2, DATE2);
 	Employee empl5 = new Employee(ID5, "name", DEP3, SALARY3, DATE3);
+
 	Employee[] employees = {empl1, empl2, empl3, empl4, empl5};
 	Company company;
 
@@ -81,48 +83,30 @@ class CompanyTest {
 
 	@Test
 	void testGetDepartmentSalaryDistribution() {
-		
-		List<DepartmentSalary> expectedList = getDepartmentSalaryDistributionList();
-		List<DepartmentSalary> actualList = company.getDepartmentSalaryDistribution();
-		DepartmentSalary [] expected = expectedList.stream().toArray(DepartmentSalary[]::new);
-		DepartmentSalary [] actual = actualList.stream().toArray(DepartmentSalary[]::new);
-		Arrays.sort(expected, (ds1, ds2) -> Double.compare(ds1.salary(), ds2.salary()));
-		Arrays.sort(actual, (ds1, ds2) -> Double.compare(ds1.salary(), ds2.salary()));
+		DepartmentSalary [] expected = {
+			new DepartmentSalary(DEP2, SALARY2),
+			new DepartmentSalary(DEP1, SALARY1),
+			new DepartmentSalary(DEP3, SALARY3)
+		};
+		DepartmentSalary [] actual = company.getDepartmentSalaryDistribution()
+				.stream().sorted((ds1, ds2) -> Double.compare(ds1.salary(), ds2.salary())).
+				toArray(DepartmentSalary[]::new);
 		assertArrayEquals(expected, actual);
-	}
-	private  List<DepartmentSalary> getDepartmentSalaryDistributionList() {
-		List<DepartmentSalary> list = new ArrayList<>();
-		list.add(new DepartmentSalary(DEP1, SALARY1));
-		list.add(new DepartmentSalary(DEP3, SALARY3));
-		list.add(new DepartmentSalary(DEP2, SALARY2));
-		return list;
 	}
 
-	private List<SalaryDistribution> getSalaryDistributionList_1() {
-		List<SalaryDistribution> list = new ArrayList<>();
-		list.add(new SalaryDistribution(4000, 5999, 2));
-		list.add(new SalaryDistribution(10000, 11999, 2));
-		list.add(new SalaryDistribution(14000, 15999, 1));	
-		return list;
-	}
-//	@Test
-	void testGetSalaryDistributionMy() {
-		
-		List<SalaryDistribution> expectedList = getSalaryDistributionList();
-		SalaryDistribution [] expected = expectedList.stream().toArray(SalaryDistribution[]::new);
-		List<SalaryDistribution> actualList = company.getSalaryDistribution(5);
-		actualList.forEach(sd -> System.out.println(sd.toString()));
-		SalaryDistribution [] actual = actualList.stream().toArray(SalaryDistribution[]::new);
-		Arrays.sort(expected, (sd1, sd2) -> Integer.compare(sd1.minSalary(), sd2.minSalary()));
-		Arrays.sort(actual, (sd1, sd2) -> Integer.compare(sd1.minSalary(), sd2.minSalary()));
+	@Test
+	void testGetSalaryDistribution() {
+		int interval = 5000;
+		SalaryDistribution[] expected = {
+				new SalaryDistribution(SALARY2, SALARY2 + interval - 1, 2),
+				new SalaryDistribution(SALARY1, SALARY1 + interval - 1, 3),
+				new SalaryDistribution(SALARY3, SALARY3 + interval - 1, 1),
+		};
+		company.addEmployee(new Employee(ID_NOT_EXIST, DEP2, DEP2, 13000,  DATE1));
+		SalaryDistribution[] actual =
+				company.getSalaryDistribution(interval)
+				.toArray(SalaryDistribution[]::new);
 		assertArrayEquals(expected, actual);
-	}
-	private List<SalaryDistribution> getSalaryDistributionList() {
-		List<SalaryDistribution> list = new ArrayList<>();
-		list.add(new SalaryDistribution(5000, 6999, 2));
-		list.add(new SalaryDistribution(9000, 10999, 2));
-		list.add(new SalaryDistribution(15000, 16999, 1));	
-		return list;
 	}
 
 	@Test
@@ -140,6 +124,74 @@ class CompanyTest {
 	void testSave() {
 		company.save(TEST_DATA);
 	}
+	
+	
+	
+	
+	@Test
+	void testGetEmployeesByDepartment() {
+	    List<Employee> expected = Arrays.asList(empl1, empl3);
+	    List<Employee> actual = company.getEmployeesByDepartment(DEP1);
+	    assertEquals(expected, actual);
+	}
+
+	
+	@Test
+	void testGetEmployeesBySalary() {
+		salaryTestRun(new Employee[] {empl1, empl3, empl2,empl4}, SALARY2, SALARY1);
+		salaryTestRun(new Employee[] {empl5}, SALARY3, SALARY3);
+		salaryTestRun(new Employee[0], 5, 3);
+	}
+	private void salaryTestRun(Employee [] expected, int salaryFrom, int salaryTo) {
+		expected = sortArrayEmployeesById(expected);
+		Employee [] actual = company.getEmployeesBySalary(salaryFrom, salaryTo).toArray(new Employee[0]);
+		actual = sortArrayEmployeesById(actual);
+		assertArrayEquals(expected, actual);
+	}
+	private Employee [] sortArrayEmployeesById(Employee[] employees) {
+		Arrays.sort(employees, (emp1, emp2) -> Long.compare(emp1.id(), emp2.id()));
+		return employees;
+	}
+////
+
+//	@Test
+//	void testGetEmployeesByAge() {
+//	    List<Employee> expected = Arrays.asList(empl2, empl3);
+//	    List<Employee> actual = company.getEmployeesByAge(30, 35);
+//	    assertEquals(expected, actual);
+//	}
+	
+	@Test
+	void testGetEmployeesByAge() {
+		ageTestRun(new Employee[] {empl1,empl3, empl5}, 0, 30);
+		ageTestRun(new Employee[0], 25, 12);
+		ageTestRun(new Employee[0], 0, 12);
+	}
+	private void ageTestRun(Employee [] expected, int ageFrom, int ageTo) {
+		expected = sortArrayEmployeesById(expected);
+		Employee [] actual = company.getEmployeesByAge(ageFrom, ageTo).toArray(new Employee[0]);
+		actual = sortArrayEmployeesById(actual);
+		assertArrayEquals(expected, actual);
+	}
+	
+
+	@Test
+	void testUpdateSalary() {
+	    Employee updatedEmployee = company.updateSalary(ID1, 12000);
+	    assertNotNull(updatedEmployee);
+	    assertEquals(12000, updatedEmployee.salary());
+	    assertEquals(updatedEmployee, company.getEmployee(ID1));
+	}
+
+	@Test
+	void testUpdateDepartment() {
+	    Employee updatedEmployee = company.updateDepartment(ID2, DEP3);
+	    assertNotNull(updatedEmployee);
+	    assertEquals(DEP3, updatedEmployee.department());
+	    assertEquals(updatedEmployee, company.getEmployee(ID2));
+	}
+
+	
+
 
 }
-//for new commit

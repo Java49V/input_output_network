30d92cbe39af2b05257cb2257bdf2acf9259c203
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-10-15 16:01:05 +0300

    HW #50 TCP_server_core

--- src/telran/net/TcpServer.java
+++ src/telran/net/TcpServer.java
@@ -1,29 +1,66 @@
 package telran.net;
+
 import java.io.IOException;
-import java.net.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class TcpServer implements Runnable {
-	private int port;
-	private ApplProtocol protocol;
-	private ServerSocket serverSocket;
-	public TcpServer(int port, ApplProtocol protocol) throws IOException {
-		this.port = port;
-		this.protocol = protocol;
-		serverSocket = new ServerSocket(port);
-	}
+    private int port;
+    private ApplProtocol protocol;
+    private ServerSocket serverSocket;
+    private ExecutorService executor;
 
-	@Override
-	public void run() {
-		System.out.println("Server is listening on port " + port);
-		try {
-			while(true) {
-				Socket socket = serverSocket.accept();
-				TcpClientServer clientServer = new TcpClientServer(socket, protocol);
-				clientServer.run();
-			}
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public TcpServer(int port, ApplProtocol protocol, int numThreads) throws IOException {
+        this.port = port;
+        this.protocol = protocol;
+        serverSocket = new ServerSocket(port);
+        executor = Executors.newFixedThreadPool(numThreads);
+    }
 
+    @Override
+    public void run() {
+        System.out.println("Server is listening on port " + port);
+        try {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                TcpClientServer clientServer = new TcpClientServer(socket, protocol);
+                executor.execute(clientServer); // Submit the task to the thread pool
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
+//import java.io.IOException;
+//import java.net.*;
+//public class TcpServer implements Runnable {
+//	private int port;
+//	private ApplProtocol protocol;
+//	private ServerSocket serverSocket;
+//	public TcpServer(int port, ApplProtocol protocol) throws IOException {
+//		this.port = port;
+//		this.protocol = protocol;
+//		serverSocket = new ServerSocket(port);
+//	}
+//
+//	@Override
+//	public void run() {
+//		System.out.println("Server is listening on port " + port);
+//		try {
+//			while(true) {
+//				Socket socket = serverSocket.accept();
+//				TcpClientServer clientServer = new TcpClientServer(socket, protocol);
+//				clientServer.run();
+//			}
+//			
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//}

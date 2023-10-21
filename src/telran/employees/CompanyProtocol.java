9884be14ea636ac0755c99b050c6142262cdead0
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-10-21 17:09:55 +0300

    HW #52 Class reflection corrected with one Return

--- src/telran/employees/CompanyProtocol.java
+++ src/telran/employees/CompanyProtocol.java
@@ -16,31 +16,139 @@ import telran.net.ResponseCode;
 
 public class CompanyProtocol implements ApplProtocol {
 
-	private Company company;
+    private Company company;
 
-	public CompanyProtocol(Company company) {
-		this.company = company;
-	}
+    public CompanyProtocol(Company company) {
+        this.company = company;
+    }
 
-	@Override
-	public Response getResponse(Request request) {
-		Serializable responseData;
-		String requestType = request.requestType();
-		Serializable data = request.requestData();
+    @Override
+    public Response getResponse(Request request) {
+        Response response;
+        String requestType = request.requestType();
+        Serializable data = request.requestData();
 
-		try {
-			Method method = this.getClass().getDeclaredMethod(requestType, Serializable.class);
-			responseData = (Serializable) method.invoke(this, data);
+        try {
+            String methodName = requestType.replace('/', '_');
+            Method method = this.getClass().getDeclaredMethod(methodName, Serializable.class);
+            Serializable responseData = (Serializable) method.invoke(this, data);
+            response = new Response(ResponseCode.OK, responseData);
+        } catch (NoSuchMethodException e) {
+            response = new Response(ResponseCode.WRONG_TYPE, requestType + " is unsupported in the Company Protocol");
+        } catch (Exception e) {
+            response = new Response(ResponseCode.WRONG_DATA, e.toString());
+        }
+        return response;
+    }
+ // This method is invoked dynamically using reflection. The IDE might flag it as unused, but it is used.
+    private Serializable salary_update(Serializable data) {
+        UpdateData<Integer> updateData = (UpdateData<Integer>) data;
+        long id = updateData.id();
+        int salary = updateData.data();
+        return company.updateSalary(id, salary);
+    }
 
-			return (responseData instanceof Response) ? (Response) responseData
-					: new Response(ResponseCode.OK, responseData);
-		} catch (NoSuchMethodException e) {
-			return new Response(ResponseCode.WRONG_TYPE, requestType + " is unsupported in the Company Protocol");
-		} catch (Exception e) {
-			return new Response(ResponseCode.WRONG_DATA, e.toString());
-		}
-	}
-	
+    private Serializable employees_age(Serializable data) {
+        FromTo fromTo = (FromTo) data;
+        int ageFrom = fromTo.from();
+        int ageTo = fromTo.to();
+        return new ArrayList<>(company.getEmployeesByAge(ageFrom, ageTo));
+    }
+
+    private Serializable employees_salary(Serializable data) {
+        FromTo fromTo = (FromTo) data;
+        int salaryFrom = fromTo.from();
+        int salaryTo = fromTo.to();
+        return new ArrayList<>(company.getEmployeesBySalary(salaryFrom, salaryTo));
+    }
+
+    private Serializable employees_department(Serializable data) {
+        String department = (String) data;
+        return new ArrayList<>(company.getEmployeesByDepartment(department));
+    }
+
+    private Serializable salary_distribution(Serializable data) {
+        int interval = (int) data;
+        return new ArrayList<>(company.getSalaryDistribution(interval));
+    }
+
+    private Serializable department_salary_distribution(Serializable data) {
+        return new ArrayList<>(company.getDepartmentSalaryDistribution());
+    }
+
+    private Serializable employee_remove(Serializable data) {
+        long id = (long) data;
+        return company.removeEmployee(id);
+    }
+
+    private Serializable department_update(Serializable data) {
+        UpdateData<String> updateData = (UpdateData<String>) data;
+        long id = updateData.id();
+        String department = updateData.data();
+        return company.updateDepartment(id, department);
+    }
+
+    private Serializable employees_get(Serializable data) {
+        return new ArrayList<>(company.getEmployees());
+    }
+
+    private Serializable employee_get(Serializable data) {
+        long id = (long) data;
+        return company.getEmployee(id);
+    }
+
+    private Serializable employee_add(Serializable data) {
+        Employee empl = (Employee) data;
+        return company.addEmployee(empl);
+    }
+}
+
+
+
+
+//package telran.employees;
+//
+//import java.io.Serializable;
+//import java.lang.reflect.Method;
+//import java.util.ArrayList;
+//
+//import telran.employees.dto.Employee;
+//import telran.employees.dto.FromTo;
+//import telran.employees.dto.SalaryDistribution;
+//import telran.employees.dto.UpdateData;
+//import telran.employees.service.Company;
+//import telran.net.ApplProtocol;
+//import telran.net.Request;
+//import telran.net.Response;
+//import telran.net.ResponseCode;
+//
+//public class CompanyProtocol implements ApplProtocol {
+//
+//	private Company company;
+//
+//	public CompanyProtocol(Company company) {
+//		this.company = company;
+//	}
+//
+////	@Override
+////	public Response getResponse(Request request) {
+////		Serializable responseData;
+////		String requestType = request.requestType();
+////		Serializable data = request.requestData();
+////
+////		try {
+////			Method method = this.getClass().getDeclaredMethod(requestType, Serializable.class);
+////			responseData = (Serializable) method.invoke(this, data);
+////
+////			return (responseData instanceof Response) ? (Response) responseData
+////					: new Response(ResponseCode.OK, responseData);
+////		} catch (NoSuchMethodException e) {
+////			return new Response(ResponseCode.WRONG_TYPE, requestType + " is unsupported in the Company Protocol");
+////		} catch (Exception e) {
+////			return new Response(ResponseCode.WRONG_DATA, e.toString());
+////		}
+////	}
+//
 //	@Override
 //	public Response getResponse(Request request) {
 //		Response response = null;
@@ -72,72 +180,72 @@ public class CompanyProtocol implements ApplProtocol {
 //		}
 //		return response;
 //	}
-
-	private Serializable salary_update(Serializable data) {
-		@SuppressWarnings("unchecked")
-		UpdateData<Integer> updateData = (UpdateData<Integer>) data;
-		long id = updateData.id();
-		int salary = updateData.data();
-		return company.updateSalary(id, salary);
-	}
-
-	private Serializable employees_age(Serializable data) {
-		FromTo fromTo = (FromTo) data;
-		int ageFrom = fromTo.from();
-		int ageTo = fromTo.to();
-		return new ArrayList<>(company.getEmployeesByAge(ageFrom, ageTo));
-	}
-
-	private Serializable employees_salary(Serializable data) {
-		FromTo fromTo = (FromTo) data;
-		int salaryFrom = fromTo.from();
-		int salaryTo = fromTo.to();
-		return new ArrayList<>(company.getEmployeesBySalary(salaryFrom, salaryTo));
-	}
-
-	private Serializable employees_department(Serializable data) {
-		String department = (String) data;
-		return new ArrayList<>(company.getEmployeesByDepartment(department));
-	}
-
-	private Serializable salary_distribution(Serializable data) {
-		int interval = (int) data;
-		return new ArrayList<>(company.getSalaryDistribution(interval));
-	}
-
-	private Serializable department_salary_distribution(Serializable data) {
-		return new ArrayList<>(company.getDepartmentSalaryDistribution());
-	}
-
-	private Serializable employee_remove(Serializable data) {
-		long id = (long) data;
-
-		return company.removeEmployee(id);
-	}
-
-	private Serializable department_update(Serializable data) {
-
-		@SuppressWarnings("unchecked")
-		UpdateData<String> updateData = (UpdateData<String>) data;
-		long id = updateData.id();
-		String department = updateData.data();
-		return company.updateDepartment(id, department);
-	}
-
-	Serializable employees_get(Serializable data) {
-
-		return new ArrayList<>(company.getEmployees());
-	}
-
-	Serializable employee_get(Serializable data) {
-		long id = (long) data;
-		return company.getEmployee(id);
-	}
-
-	Serializable employee_add(Serializable data) {
-
-		Employee empl = (Employee) data;
-		return company.addEmployee(empl);
-	}
-
-}
+//
+//	private Serializable salary_update(Serializable data) {
+//		@SuppressWarnings("unchecked")
+//		UpdateData<Integer> updateData = (UpdateData<Integer>) data;
+//		long id = updateData.id();
+//		int salary = updateData.data();
+//		return company.updateSalary(id, salary);
+//	}
+//
+//	private Serializable employees_age(Serializable data) {
+//		FromTo fromTo = (FromTo) data;
+//		int ageFrom = fromTo.from();
+//		int ageTo = fromTo.to();
+//		return new ArrayList<>(company.getEmployeesByAge(ageFrom, ageTo));
+//	}
+//
+//	private Serializable employees_salary(Serializable data) {
+//		FromTo fromTo = (FromTo) data;
+//		int salaryFrom = fromTo.from();
+//		int salaryTo = fromTo.to();
+//		return new ArrayList<>(company.getEmployeesBySalary(salaryFrom, salaryTo));
+//	}
+//
+//	private Serializable employees_department(Serializable data) {
+//		String department = (String) data;
+//		return new ArrayList<>(company.getEmployeesByDepartment(department));
+//	}
+//
+//	private Serializable salary_distribution(Serializable data) {
+//		int interval = (int) data;
+//		return new ArrayList<>(company.getSalaryDistribution(interval));
+//	}
+//
+//	private Serializable department_salary_distribution(Serializable data) {
+//		return new ArrayList<>(company.getDepartmentSalaryDistribution());
+//	}
+//
+//	private Serializable employee_remove(Serializable data) {
+//		long id = (long) data;
+//
+//		return company.removeEmployee(id);
+//	}
+//
+//	private Serializable department_update(Serializable data) {
+//
+//		@SuppressWarnings("unchecked")
+//		UpdateData<String> updateData = (UpdateData<String>) data;
+//		long id = updateData.id();
+//		String department = updateData.data();
+//		return company.updateDepartment(id, department);
+//	}
+//
+//	Serializable employees_get(Serializable data) {
+//
+//		return new ArrayList<>(company.getEmployees());
+//	}
+//
+//	Serializable employee_get(Serializable data) {
+//		long id = (long) data;
+//		return company.getEmployee(id);
+//	}
+//
+//	Serializable employee_add(Serializable data) {
+//
+//		Employee empl = (Employee) data;
+//		return company.addEmployee(empl);
+//	}
+//
+//}
